69cfd219f6c2fbaecf7e12a556884233a585b182
Numan Karaaslan <numan4634@hotmail.com>
2021-03-11 21:31:03 +0300

    Pom updates

---
+++ ab - mailservice/src/main/java/com/aldimbilet/mailservice/AldimbiletMailServicesApplication.java
@@ -0,0 +1,13 @@
+package com.aldimbilet.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AldimbiletMailServicesApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AldimbiletMailServicesApplication.class, args);
+	}
+}
